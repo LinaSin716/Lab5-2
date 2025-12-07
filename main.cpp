f2882fabf1acf2b815527f9576adbcc7cc4fae35
Daria <daria.samsonova06@mail.ru>
2025-12-07 19:37:46 +0500

    Функция 3 - ToNewFile - записывает строки в файл

--- main.cpp
+++ main.cpp
@@ -17,15 +17,20 @@ void PrintVector(const vector<string>& lines) {
 	}
 }
 
-void ToNewFile() {};
+void ToNewFile(const vector<string>& lines, const string& file1) {
+	ofstream file(file1);
+	for (size_t i = 0; i < lines.size(); i++) {
+		file << lines[i] << endl;
+	}
+}
 
 int main() {
 
 	ifstream meowFile("meowFile.txt");
-
 	vector<string> lines = FromFile();
+	
 	PrintVector(lines);
-	ToNewFile();
+	ToNewFile(lines, "new.txt");
 
 	return 0;
 }
